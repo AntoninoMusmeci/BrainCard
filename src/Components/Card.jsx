13e6e315eef1dd49263fa9c1887062910c48cf07
Antonino Musmeci <nino.musmeci@gmail.com>
2022-10-04 19:16:57 +0200

    create card component

---
+++ src/Components/Card.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "../styles.css";
+import styled from "styled-components"
+export default function Card({front,back}) {
+  const [flip, setFlip] = useState(false);
+  const handleClick = () => {
+    setFlip(!flip);
+  };
+  return (
+
+      <CardStyle className={`card ${flip ? "flip" : ""}`}>
+        <div className="front" onClick={handleClick}>
+          {front}
+        </div>
+        <div className="back" onClick={handleClick}>
+        {back}
+        </div>
+      </CardStyle>
+ 
+  );
+}
+
+const CardStyle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  border-radius: 0.4rem;
+  box-shadow: 0 0 5px 5px rgba(0,0,0,0.5);
+  background-color: #f1f1f1;
+`
